8e82053a7cc1208466b627cb848c74cab9a50a6a
Carabus Manuel Alexandru <manue971@icloud.com>
2020-05-29 22:10:38 +0300

    adding stripe payment to buddypress tabs

--- wp-content/themes/masterstudy-child/functions.php
+++ wp-content/themes/masterstudy-child/functions.php
@@ -1,6 +1,15 @@
 <?php
 
-function profile_tab_yourtabname() {
+if( is_user_logged_in() ) {
+    $user = wp_get_current_user();
+    $roles = ( array ) $user->roles;
+
+    if (in_array('administrator', $roles) || in_array('stm_lms_instructor', $roles)) {
+        add_action( 'bp_setup_nav', 'woocommerce_payment' );
+    }
+}
+
+function woocommerce_payment() {
     global $bp;
 
     bp_core_new_nav_item( array( 
@@ -12,21 +21,20 @@ function profile_tab_yourtabname() {
           'parent_slug'     => $bp->profile->slug
     ) );
 }
-add_action( 'bp_setup_nav', 'profile_tab_yourtabname' );
 
 
 function trigger_woocommerce_payment() {
   
   // Add title and content here - last is to call the members plugin.php template.
-  add_action( 'bp_template_title', 'trigger_woocommerce_payment_title' );
-  add_action( 'bp_template_content', 'trigger_woocommerce_payment_content' );
+  add_action( 'bp_template_title', 'woocommerce_payment_title' );
+  add_action( 'bp_template_content', 'woocommerce_payment_content' );
   bp_core_load_template( 'buddypress/members/single/plugins' );
 }
-function trigger_woocommerce_payment_title() {
+function woocommerce_payment_title() {
     echo _x( 'Stripe Connect', 'No need translation...', 'yith-stripe-connect-for-woocommerce' );
 }
 
-function trigger_woocommerce_payment_content() { 
+function woocommerce_payment_content() { 
     do_action('woocommerce_account_stripe-connect_endpoint');
 }
 
